9659f6584db04b730c2db9ce552e20b72d93c75e
alejandro <alejandrober96@gmail.com>
2023-12-04 15:45:07 -0500

    Add product model

---
+++ src/models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+    min: 3,
+    max: 200,
+  },
+  offer: {
+    type: Number,
+    max: 100,
+    min: 0,
+  },
+});
+
+const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
+module.exports = Product;
