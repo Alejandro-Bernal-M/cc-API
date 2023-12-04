329f6f97286c237cf03d3d27db7ab949c6bcd96c
alejandro <alejandrober96@gmail.com>
2023-12-04 12:00:55 -0500

    Add auth middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', authRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port = ${process.env.PORT}`);
 });
