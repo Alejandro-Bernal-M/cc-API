2994f3b0f1424169aab8110c82e37e072b97035c
alejandro <alejandrober96@gmail.com>
2023-12-04 16:44:27 -0500

    update product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,32 +1,51 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    productImages: [
+      { img: String },
+    ],
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      min: 3,
+      max: 200,
+    },
+    offer: {
+      type: Number,
+      max: 100,
+      min: 0,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    updatedAt: Date,
   },
-  productImages: [
-    { img: String },
-  ],
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
+  {
+    timestamps: true,
   },
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-    min: 3,
-    max: 200,
-  },
-  offer: {
-    type: Number,
-    max: 100,
-    min: 0,
-  },
-});
+);
 
 const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
 module.exports = Product;
